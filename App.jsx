b9f850f190263cc4c5987724d271b4605688f814
Hareem Fatima <129002691+hareem2806@users.noreply.github.com>
2024-12-16 00:36:21 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// Import Components
+import LoginPage from './LoginPage';
+import HomePage from './HomePage'; // Your home page component
+
+// Main App Component
+const App = () => {
+  return (
+    <div>
+      {/* <HomePage /> */}
+      <LoginPage />
+    </div>
+  );
+};
+
+export default App;
